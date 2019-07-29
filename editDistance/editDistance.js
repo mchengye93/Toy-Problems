90a39855423a024e43fa02ad658a62a232c0975c
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-28 22:19:42 -0700

    MVP edit distance

--- editDistance/editDistance.js
+++ editDistance/editDistance.js
@@ -4,6 +4,27 @@
 // Challenge: Do this in O(m x n) time, where m, n are the respective lengths of str1 and str2.
 
 function editDistance(str1, str2) {
-  // Your code here.
+ 
 
-}
+  if(a.length === 0) return b.length; 
+  if(b.length === 0) return a.length; 
+  var matrix = [], i, j;
+  for(i = 0; i <= b.length; i++){
+    matrix[i] = [i];
+  }
+  for(j = 0; j <= a.length; j++){
+    matrix[0][j] = j;
+  }
+  for(i = 1; i <= b.length; i++){
+    for(j = 1; j <= a.length; j++){
+      if(b.charAt(i-1) == a.charAt(j-1)){
+        matrix[i][j] = matrix[i-1][j-1];
+      } else {
+        matrix[i][j] = Math.min(
+          matrix[i-1][j-1] + 1,
+          matrix[i][j-1] + 1, 
+          matrix[i-1][j] + 1  )
+      }
+    }
+  }
+  return matrix[b.length][a.length];
